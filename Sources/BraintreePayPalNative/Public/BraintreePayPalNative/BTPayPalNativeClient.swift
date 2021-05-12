4c74ac99663743a22248d50d6ff0896493d29d38
Susan Stevens <susan.stevens@getbraintree.com>
2021-05-12 13:39:14 -0500

    WIP - adding BTPayPalNativeClient

--- Sources/BraintreePayPalNative/Public/BraintreePayPalNative/BTPayPalNativeClient.swift
+++ Sources/BraintreePayPalNative/Public/BraintreePayPalNative/BTPayPalNativeClient.swift
@@ -5,6 +5,28 @@ import BraintreeCore
     let apiClient: BTAPIClient
 
     /**
+     Domain for PayPal errors.
+     */
+    // TODO: - is this the right naming convention?
+    @objc public static let errorDomain = "com.braintreepayments.BTPayPalNativeClientErrorDomain"
+
+    /**
+     Error codes associated with PayPal.
+     */
+    @objc public enum ErrorType: Int {
+        /// Unknown error
+        case unknown
+        /// PayPal is disabled in configuration
+        case disabled
+        /// Invalid request, e.g. missing PayPal request
+        case invalidRequest
+        /// Braintree SDK is integrated incorrectly
+        case integration
+        /// Payment flow was canceled, typically initiated by the user when exiting early from the flow.
+        case canceled
+    }
+
+    /**
      Initializes a PayPal client.
 
      - Parameter apiClient: The Braintree API client
@@ -27,7 +49,42 @@ import BraintreeCore
      @param completionBlock This completion will be invoked exactly once when tokenization is complete or an error occurs.
     */
     // TODO: - use Error instead of NSError?
-    @objc public func tokenizePayPalAccountWithPayPalNativeRequest(request: BTPayPalNativeRequest, completion: (BTPayPalNativeAccountNonce?, NSError?) -> Void) {
+    @objc(tokenizePayPalAccountWithPayPalRequest:completion:)
+    public func tokenizePayPalAccount(with request: BTPayPalNativeRequest, completion: @escaping (BTPayPalNativeAccountNonce?, NSError?) -> Void) {
+        guard request is BTPayPalNativeCheckoutRequest || request is BTPayPalNativeVaultRequest else {
+            let error = NSError(domain: BTPayPalNativeClient.errorDomain,
+                                code: ErrorType.integration.rawValue,
+                                userInfo: [NSLocalizedDescriptionKey: "BTPayPalNativeClient failed because request is not of type BTPayPalNativeCheckoutRequest or BTPayPalNativeVaultRequest."])
 
+            completion(nil, error)
+            return
+        }
+
+        apiClient.fetchOrReturnRemoteConfiguration { config, error in
+            if let err = error as NSError? {
+                completion(nil, err)
+                return
+            }
+
+            // check if configuration is nil
+
+            if !config.json["paypalEnabled"].isTrue {
+
+            }
+
+//            if (![configuration[@"paypalEnabled"] isTrue]) {
+//                [self.apiClient sendAnalyticsEvent:@"ios.paypal-otc.preflight.disabled"];
+//                if (error != NULL) {
+//                    *error = [NSError errorWithDomain:BTPayPalDriverErrorDomain
+//                                                 code:BTPayPalDriverErrorTypeDisabled
+//                                             userInfo:@{ NSLocalizedDescriptionKey: @"PayPal is not enabled for this merchant",
+//                                                         NSLocalizedRecoverySuggestionErrorKey: @"Enable PayPal for this merchant in the Braintree Control Panel" }];
+//                }
+//                return NO;
+//            }
+//
+//            return YES;
+
+        }
     }
 }
--- UnitTests/BraintreePayPalNativeTests/BTPayPalNativeClient_Tests.swift
+++ UnitTests/BraintreePayPalNativeTests/BTPayPalNativeClient_Tests.swift
@@ -1,21 +1,65 @@
 import XCTest
+import BraintreeCore
 import BraintreeTestShared
 @testable import BraintreePayPalNative
 
 class BTPayPalNativeClient_Tests: XCTestCase {
 
-    func testTokenize_whenRequestIsNotCheckoutOrVaultSubclass_returnsError() {
-        let mockAPIClient = MockAPIClient(authorization: "development_tokenization_key")!
-        let payPalNativeClient = BTPayPalNativeClient(apiClient: mockAPIClient)
+    private var mockAPIClient: MockAPIClient!
+    private var payPalNativeClient: BTPayPalNativeClient!
 
+    override func setUp() {
+        mockAPIClient = MockAPIClient(authorization: "development_tokenization_key")!
+        payPalNativeClient = BTPayPalNativeClient(apiClient: mockAPIClient)
+    }
+
+    func testTokenize_whenRequestIsNotCheckoutOrVaultSubclass_returnsError() {
         let expectation = self.expectation(description: "calls completion with error")
-        payPalNativeClient.tokenizePayPalAccountWithPayPalNativeRequest(request: BTPayPalNativeRequest()) { nonce, error in
+        payPalNativeClient.tokenizePayPalAccount(with: BTPayPalNativeRequest()) { nonce, error in
             XCTAssertNil(nonce)
             XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription, "BTPayPalNativeClient failed because request is not of type BTPayPalNativeCheckoutRequest or BTPayPalNativeVaultRequest.")
 
             expectation.fulfill()
         }
 
         waitForExpectations(timeout: 1)
+    }
+
+    func testTokenizePayPalAccount_whenRemoteConfigurationFetchFails_callsBackWithConfigurationError() {
+        mockAPIClient.cannedConfigurationResponseBody = nil
+        mockAPIClient.cannedConfigurationResponseError = NSError(domain: "", code: 0, userInfo: nil)
+
+        let request = BTPayPalNativeCheckoutRequest(amount: "1")
+        let expectation = self.expectation(description: "Checkout fails with error")
+
+        payPalNativeClient.tokenizePayPalAccount(with: request) { (nonce, error) in
+            guard let error = error as NSError? else { XCTFail(); return }
+            XCTAssertNil(nonce)
+            XCTAssertEqual(error, self.mockAPIClient.cannedConfigurationResponseError)
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 1)
+    }
+
+    func testTokenizePayPalAccount_whenPayPalNotEnabledInConfiguration_callsBackWithError() {
+        mockAPIClient.cannedConfigurationResponseBody = BTJSON(value: [
+            "paypalEnabled": false
+        ])
+
+        let request = BTPayPalNativeCheckoutRequest(amount: "1")
+        let expectation = self.expectation(description: "Checkout fails with error")
+
+        payPalNativeClient.tokenizePayPalAccount(with: request) { (nonce, error) in
+            guard let error = error as NSError? else { XCTFail(); return }
+            XCTAssertNil(nonce)
+            XCTAssertEqual(error.domain, BTPayPalNativeClient.errorDomain)
+            XCTAssertEqual(error.code, BTPayPalNativeClient.ErrorType.disabled.rawValue)
+            XCTAssertEqual(error.localizedDescription, "PayPal is not enabled for this merchant")
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 1)
     }
 }
